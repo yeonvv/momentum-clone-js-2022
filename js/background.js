b181f10b912b98d734c43a10663a608e89e12ed2
HeoYeonJae <yeonvv8@gmail.com>
2022-03-16 20:58:46 +0900

    Random bg finished

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["1.jpg", "2.jpg", "3.jpg"];
+
+const randomImg = images[Math.floor(Math.random() * images.length)];
+
+const randomBgImg = document.createElement("img");
+
+randomBgImg.src = `img/${randomImg}`;
+
+document.body.appendChild(randomBgImg);
