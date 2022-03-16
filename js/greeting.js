c081cd22c080d007b2b929cdd6f5195a4c517d44
HeoYeonJae <yeonvv8@gmail.com>
2022-03-16 09:08:41 +0900

    greeting finished
    rename, and divide and conquer

--- js/greeting.js
+++ js/greeting.js
@@ -5,16 +5,16 @@ const greeting = document.getElementById("greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreeting(username);
-}
-function paintGreeting(username) {
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
