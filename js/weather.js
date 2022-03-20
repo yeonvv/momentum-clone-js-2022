acc8e8346348553d5cdc80b9e052533f9f7a4101
HeoYeonJae <yeonvv8@gmail.com>
2022-03-20 18:30:50 +0900

    weather finished

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+const API_KEY = "0a228b51fcd2376bec385eeaa434048f";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const long = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`;
+  fetch(url).then((Response) =>
+    Response.json().then((data) => {
+      const weather = document.getElementById("weather");
+      const curWeather = weather.querySelector("span:first-child");
+      const curCity = weather.querySelector("span:nth-child(2)");
+      const temp = document.createElement("span");
+      weather.appendChild(temp);
+      curWeather.innerText = data.name;
+      curCity.innerText = data.weather[0].main;
+      temp.innerText = `/${data.main.temp}`;
+    })
+  );
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
